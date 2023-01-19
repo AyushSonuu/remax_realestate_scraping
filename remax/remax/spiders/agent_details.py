564bea3e09881272f571c9d3aa0685a7248ccaa6
ayush <sonuayush55@gmail.com>
2023-01-20 00:08:59 +0530

    today

--- remax/remax/spiders/agent_details.py
+++ remax/remax/spiders/agent_details.py
@@ -24,9 +24,11 @@ class url_extracter_class:
             city = dic["zips"][0]["city"].lower()
             city = city.replace(" ","-")
             end_url = city+"-"+state
-            url = f"https://www.remax.com/real-estate-agents/{end_url}"
+            url_ = f"https://www.remax.com/real-estate-agents/{end_url}"
+            end = '?filters={"page":1}'
+            url = url_+ end
             url_lst.append(url)
-            print(city)
+            # print(city)
 
         return url_lst
 
@@ -42,12 +44,14 @@ class AgentDetailsSpider(scrapy.Spider,url_extracter_class):
     def start_requests(self):
         zip_codes = ["80237","55125","80111","20016","92604"]
         url_lst = url_extracter_class.urls_to_send(zip_codes)
-        yield(SeleniumRequest(url = url_lst[0],headers={
-                'User-Agent': self.user_agent
-                },  callback = self.parse,
-                    wait_time = 5,
-                    screenshot=True,
-                    ))
+        for ur in url_lst:
+            yield(SeleniumRequest(url = ur,headers={
+                    'User-Agent': self.user_agent
+                    },  callback = self.parse,
+                        wait_time = 5,
+                        screenshot=True,
+                        ))
+            break
 
     def parse(self, response):
         driver =  response.request.meta['driver']
@@ -56,11 +60,53 @@ class AgentDetailsSpider(scrapy.Spider,url_extracter_class):
         for agents in response.selector.xpath("//div[@class='info']/a"):
             agent_relative_url = agents.xpath(".//@href").get()
             agent_absolute_url = f"https://www.remax.com{agent_relative_url}"
-            yield SeleniumRequest(url=agent_absolute_url,callback=self._agent_,
+            yield SeleniumRequest(url=agent_absolute_url,callback=self.agent_,
                                     headers={
                                     'User-Agent': self.user_agent
                                     },meta = {'Agent URL':agent_absolute_url},)
             
         # with open('image.png', 'wb') as image_file:
         #     image_file.write(response.meta['screenshot'])
+
+    def agent_(self,response):
+        adress = response.selector.xpath("//*[@id='__layout']/div/main/article/section/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a/text()").getall()
+        
+        adr_lst = []
+        for adr in adress:
+            adr_lst.append(adr.strip())
+        
+
+        phones = response.selector.xpath("//*[@id='__layout']/div/main/article/section/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/h4[1]/span/a/text()").getall()
+        
+        phone_dic = {}
+        for i,phone in enumerate(phones):
+            p = phone.strip()
+            key = f"phone {i}"
+            phone_dic[key] = p
+
+
+        # if len(phone_lst) == 1:
+        #     phone1 = phone_lst[0]
+        # elif len(phone_lst) == 2:
+        #     phone2 = phone_lst[1]
+        # elif len(phone_lst) == 3:
+        #     phone3 = phone_lst[2]
+        # else:
+        #     pass
+
+        yield{
+            "Agent url":response.meta.get("Agent URL"),
+            "Name":response.selector.xpath("normalize-space(//*[@id='__layout']/div/main/article/section/div/div[1]/h1/text())").get(),
+            "Profile Pic":response.selector.xpath("//*[@id='__layout']/div/main/article/section/div/div[1]/div[2]/div[1]/div/div[1]/img/@src").get(),
+            "Role":response.selector.xpath("//*[@id='__layout']/div/main/article/section/div/div[1]/div[1]/h6/span[1]/text()").get(),
+            "Lic No":response.selector.xpath("normalize-space(//*[@id='__layout']/div/main/article/section/div/div[1]/div[1]/h6/span[3]/text())").get()[9:],
+            "Address":adr_lst[0]+" "+adr_lst[1],
+            "Office Name":response.selector.xpath("normalize-space(//*[@id='__layout']/div/main/article/section/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/h4/a/text())").get(),
+            "Zip": (adr_lst[0]+" "+adr_lst[1])[-5:],
+            "Phone 1": phone_dic.get("phone 0"),
+            "Phone 2": phone_dic.get("phone 1"),
+            "Phone 3": phone_dic.get("phone 2"),
+        }
+
+
 
